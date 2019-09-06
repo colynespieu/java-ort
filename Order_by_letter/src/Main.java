bb5b28f0ee2e0cad2067250426b6310325f683f2
Colyn <colyn@espieu.com>
2019-09-06 07:38:19 +0200

    ajout prob majuscule

--- Order_by_letter/src/Main.java
+++ Order_by_letter/src/Main.java
@@ -2,19 +2,17 @@
 public class Main {
 	public static void main(String[] args) {
 		
-		String[] words = {"salut","werleyyyy","python","is","better","ma","voici","good-year","konnichiwa","hello","tata","coco","crayon","stylo","feutre","taille-crayon","pointe","mine","gomme","dessin","coloriage","rayure","peinture","pinceau","couleur","craie","papier","feuille","cahier","carnet","carton","ciseaux","découpage","pliage","plier","colle","affaire","boîte","casier","caisse","trousse","cartable","jouet","jeu"};
+		String[] words = {"salut","Test Maj Space","werleyyyy","python","is","better","ma","matrice","voici","good-year","konnichiwa","hello","tata","coco","crayon","stylo","feutre","taille-crayon","pointe","mine","gomme","dessin","coloriage","rayure","peinture","pinceau","couleur","craie","papier","feuille","cahier","carnet","carton","ciseaux","découpage","pliage","plier","colle","affaire","boîte","casier","caisse","trousse","cartable","jouet","jeu"};
 		boolean change = true;
 		String[] tmplist = new String[2];
 		
-		while (change == true) {
+		while (change) {
 			change = false;
 			
 			for (int indice = 0 ; indice < words.length-1 ; indice++ ) {
-				tmplist = changeOrder(words[indice] ,words[indice+1] );
-				System.out.println(words[indice]);
-				System.out.println(tmplist[0]);
-				if (tmplist[0].equals(words[indice]) ) {
-					words[indice] = tmplist[1]; words[indice+1] = tmplist[0];
+				tmplist = setOrder(words[indice] ,words[indice+1] );
+				if (tmplist[0].equals(words[indice+1]) ) {
+					words[indice] = tmplist[0]; words[indice+1] = tmplist[1];
 					change = true;
 				}
 			}
@@ -25,7 +23,7 @@ public class Main {
 		}
 	}
 	
-	public static String[] changeOrder(String word1 , String word2 ) {
+	public static String[] setOrder(String word1 , String word2 ) {
 		
 		char[] alphaorder = { '-',' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 		String[] twoorder = new String[2];
@@ -38,20 +36,32 @@ public class Main {
 			maxcount = word1.length();
 		}
 		for (int i = 0 ; i < maxcount ; i++) {
+			//Verification de chaque lettre de l'alphabet
 			for (int z = 0;z < alphaorder.length;z++ ) {
-				if (word1.charAt(i) == alphaorder[z] ) {
+				if (word1.toLowerCase().charAt(i) == alphaorder[z] ) {
 					iword1 = z;
 				}
-				if (word2.charAt(i) == alphaorder[z] ) {
+				if (word2.toLowerCase().charAt(i) == alphaorder[z] ) {
 					iword2 = z;
 				}
 			}
+			//Retour des mots dans le bon ordre
 			if (iword1 != iword2) {
-				if (iword1 < iword2 ) {
+				if (iword1 > iword2) {
 					twoorder[0] = word2; twoorder[1] = word1; 
 				}
-				else {
+				else if (iword2 > iword1) {
 					twoorder[0] = word1; twoorder[1] = word2; 
+				}
+				return twoorder;
+			}
+			//Retour mots plus court l'un que l'autre
+			else if (iword1 == iword2 && i == maxcount-1) {
+				if (word1.length() < word2.length()) {
+					twoorder[0] = word1; twoorder[1] = word2;
+				}
+				if (word2.length() < word1.length()) {
+					twoorder[0] = word2; twoorder[1] = word1;
 				}
 				return twoorder;
 			}
